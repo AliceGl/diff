7222460885f509f33aa0ff424d2b112ff30bd016
AliceGl <alice2003b@gmail.com>
2021-09-10 23:11:57 +0300

    added function longestCommonSubsequence and tests for it

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,42 @@
+import kotlin.math.max
+
+/* function longestCommonSubsequence finds LCS of arrays a and b
+ and returns array that contains pairs of indexes that elements of LCS
+ have in arrays a and b
+ */
+fun <T> longestCommonSubsequence (a: Array<T>, b: Array<T>): Array<Pair<Int, Int>> {
+    val n = a.size
+    val m = b.size
+    val lcsDP: Array<Array<Int>> = Array(n + 1) { Array(m + 1) {0} }
+    //lcs[i][j] - LCS for a[0..i - 1] and b[0..j - 1]
+    for (i in 1..n) {
+        for (j in 1..m) {
+            if (a[i - 1] == b[j - 1])
+                lcsDP[i][j] = lcsDP[i - 1][j - 1] + 1
+            else
+                lcsDP[i][j] = max(lcsDP[i - 1][j], lcsDP[i][j - 1])
+        }
+    }
+    val lcsSize = lcsDP[n][m]
+    val lcs : Array<Pair<Int, Int>> = Array(lcsSize) { Pair(0, 0) }
+
+    //finding LCS's indexes in a and b
+    var i = n
+    var j = m
+    var curPair = lcsSize - 1
+    while (curPair >= 0) {
+        if (a[i - 1] == b[j - 1]) {
+            lcs[curPair] = Pair(i - 1, j - 1)
+            --i; --j; --curPair
+        } else if (lcsDP[i][j] == lcsDP[i - 1][j])
+            --i
+        else
+            --j
+    }
+
+    return lcs
+}
+
 fun main(args: Array<String>) {
     TODO()
 }
--- src/test/kotlin/Test1.kt
+++ src/test/kotlin/Test1.kt
@@ -3,7 +3,21 @@ import kotlin.test.*
 internal class Test1 {
 
     @Test
-    fun test1() {
-        assert(true)
+    fun lcsTests() {
+        assertContentEquals(arrayOf(Pair(1, 1), Pair(2, 4), Pair(3, 5), Pair(4, 7)),
+            longestCommonSubsequence(arrayOf(1, 2, 3, 4, 5), arrayOf(7, 2, 4, 5, 3, 4, 9, 5)))
+
+        assertContentEquals(arrayOf(Pair(0, 1), Pair(3, 2), Pair(4, 3)),
+            longestCommonSubsequence(arrayOf("aaa", "b", "ab", "ababa", "b"), arrayOf("b", "aaa", "ababa", "b")))
+
+        assertContentEquals(arrayOf(Pair(0, 1), Pair(2, 2), Pair(4, 4), Pair(7, 5)),
+            longestCommonSubsequence(arrayOf(1, 2, 3, 1, 2, 1, 1, 3), arrayOf(2, 1, 3, 4, 2, 3)))
+
+        assertContentEquals(arrayOf(Pair(0, 1), Pair(1, 2), Pair(2, 3)),
+            longestCommonSubsequence(arrayOf(5, 5, 5, 4), arrayOf(4, 5, 5, 5)))
+
+        assertContentEquals(arrayOf(Pair(0, 0), Pair(1, 1), Pair(2, 2), Pair(3, 3), Pair(4, 4), Pair(5, 5), Pair(6, 6)),
+            longestCommonSubsequence(arrayOf(1, 1, 1, 1, 1, 1, 1), arrayOf(1, 1, 1, 1, 1, 1, 1)))
+
     }
 }
