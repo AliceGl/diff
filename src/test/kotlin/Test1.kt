4bffcb97a60308fda3c7bf84e47831dac098f9bc
AliceGl <alice2003b@gmail.com>
2021-09-17 14:02:47 +0300

    fixed tests

--- src/test/kotlin/Test1.kt
+++ src/test/kotlin/Test1.kt
@@ -4,20 +4,20 @@ internal class Test1 {
 
     @Test
     fun lcsTests() {
-        assertContentEquals(arrayOf(Pair(2, 2), Pair(3, 5), Pair(4, 6), Pair(5, 8)),
-            longestCommonSubsequence(arrayOf(1, 2, 3, 4, 5), arrayOf(7, 2, 4, 5, 3, 4, 9, 5)))
+        assertEquals(listOf(Pair(2, 2), Pair(3, 5), Pair(4, 6), Pair(5, 8)),
+            longestCommonSubsequence(listOf(1, 2, 3, 4, 5), listOf(7, 2, 4, 5, 3, 4, 9, 5)))
 
-        assertContentEquals(arrayOf(Pair(1, 2), Pair(4, 3), Pair(5, 4)),
-            longestCommonSubsequence(arrayOf("aaa", "b", "ab", "ababa", "b"), arrayOf("b", "aaa", "ababa", "b")))
+        assertEquals(listOf(Pair(1, 2), Pair(4, 3), Pair(5, 4)),
+            longestCommonSubsequence(listOf("aaa", "b", "ab", "ababa", "b"), listOf("b", "aaa", "ababa", "b")))
 
-        assertContentEquals(arrayOf(Pair(1, 2), Pair(3, 3), Pair(5, 5), Pair(8, 6)),
-            longestCommonSubsequence(arrayOf(1, 2, 3, 1, 2, 1, 1, 3), arrayOf(2, 1, 3, 4, 2, 3)))
+        assertEquals(listOf(Pair(1, 2), Pair(3, 3), Pair(5, 5), Pair(8, 6)),
+            longestCommonSubsequence(listOf(1, 2, 3, 1, 2, 1, 1, 3), listOf(2, 1, 3, 4, 2, 3)))
 
-        assertContentEquals(arrayOf(Pair(1, 2), Pair(2, 3), Pair(3, 4)),
-            longestCommonSubsequence(arrayOf(5, 5, 5, 4), arrayOf(4, 5, 5, 5)))
+        assertEquals(listOf(Pair(1, 2), Pair(2, 3), Pair(3, 4)),
+            longestCommonSubsequence(listOf(5, 5, 5, 4), listOf(4, 5, 5, 5)))
 
-        assertContentEquals(arrayOf(Pair(1, 1), Pair(2, 2), Pair(3, 3), Pair(4, 4), Pair(5, 5), Pair(6, 6), Pair(7, 7)),
-            longestCommonSubsequence(arrayOf(1, 1, 1, 1, 1, 1, 1), arrayOf(1, 1, 1, 1, 1, 1, 1)))
+        assertEquals(listOf(Pair(1, 1), Pair(2, 2), Pair(3, 3), Pair(4, 4), Pair(5, 5), Pair(6, 6), Pair(7, 7)),
+            longestCommonSubsequence(listOf(1, 1, 1, 1, 1, 1, 1), listOf(1, 1, 1, 1, 1, 1, 1)))
 
     }
 
@@ -28,14 +28,14 @@ internal class Test1 {
             DiffBlock(2, 0, 2, 2),
             DiffBlock(4, 0, 6, 1)
         ),
-            findDiff(arrayOf(Pair(2, 2), Pair(3, 5), Pair(4, 6), Pair(5, 8)), 5, 8)
+            findDiff(listOf(Pair(2, 2), Pair(3, 5), Pair(4, 6), Pair(5, 8)), 5, 8)
         )
 
         assertEquals(listOf(
             DiffBlock(0, 0, 0, 1),
             DiffBlock(1, 2, 2, 0),
         ),
-            findDiff(arrayOf(Pair(1, 2), Pair(4, 3), Pair(5, 4)), 5, 4)
+            findDiff(listOf(Pair(1, 2), Pair(4, 3), Pair(5, 4)), 5, 4)
         )
 
         assertEquals(listOf(
@@ -44,18 +44,18 @@ internal class Test1 {
             DiffBlock(3, 1, 3, 1),
             DiffBlock(5, 2, 5, 0),
         ),
-            findDiff(arrayOf(Pair(1, 2), Pair(3, 3), Pair(5, 5), Pair(8, 6)), 8, 6)
+            findDiff(listOf(Pair(1, 2), Pair(3, 3), Pair(5, 5), Pair(8, 6)), 8, 6)
         )
 
         assertEquals(listOf(
             DiffBlock(0, 0, 0, 1),
             DiffBlock(3, 1, 4, 0),
         ),
-            findDiff(arrayOf(Pair(1, 2), Pair(2, 3), Pair(3, 4)), 4, 4)
+            findDiff(listOf(Pair(1, 2), Pair(2, 3), Pair(3, 4)), 4, 4)
         )
 
         assertEquals(listOf(),
-            findDiff(arrayOf(Pair(1, 1), Pair(2, 2), Pair(3, 3), Pair(4, 4), Pair(5, 5),
+            findDiff(listOf(Pair(1, 1), Pair(2, 2), Pair(3, 3), Pair(4, 4), Pair(5, 5),
                 Pair(6, 6), Pair(7, 7)), 7, 7)
         )
     }
